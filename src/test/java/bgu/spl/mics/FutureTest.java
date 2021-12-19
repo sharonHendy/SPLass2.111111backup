145c64ac0aadb6765c14aa0e394e057a6fc56975
sharonHendy <93990675+sharonHendy@users.noreply.github.com>
2021-12-19 13:43:05 +0200

    !!!!!!!!!!!!!1

--- src/test/java/bgu/spl/mics/FutureTest.java
+++ src/test/java/bgu/spl/mics/FutureTest.java
@@ -1,8 +1,7 @@
 package bgu.spl.mics;
 
 import bgu.spl.mics.example.messages.ExampleEvent;
-import junit.framework.TestCase;
-import org.junit.After;
+import bgu.spl.mics.example.services.ExampleMicroService;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,8 +10,9 @@ import java.time.temporal.ChronoUnit;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import static org.junit.Assert.*;
 
-public class FutureTest extends TestCase {
+public class FutureTest {
     private static MessageBus mb;
     private static MicroService ms;
     private static Future<String> future;
@@ -22,30 +22,31 @@ public class FutureTest extends TestCase {
     public void setUp(){
         //creates event, future for that event, messageBus and MicroService
         e = new ExampleEvent("test");
-        future = mb.sendEvent(e);
         mb = MessageBusImpl.getInstance();
         ms = new ExampleMicroService("name");
         mb.register(ms);
+        mb.subscribeEvent(e.getClass(),ms);
+        future = mb.sendEvent(e);
     }
-
-    @After
-    public void tearDown() throws Exception {
-    }
-
     @Test
     public void get() {
         AtomicBoolean flag = new AtomicBoolean(false);
         Thread t = new Thread(() -> {future.resolve("result"); flag.set(true);});
         t.start();
-        //String result = future.get();
+        try{
+            t.join();
+        }catch (InterruptedException ignore){}
+        String result = future.get();
 
-        //assertTrue("result".equals(result) && flag.get()); //checks that the result was placed only when future has been resolved
+        assertTrue("result".equals(result) && flag.get()); //checks that the result was placed only when future has been resolved
     }
 
     @Test
     public void resolve() {
         future.resolve("result");
-        //assertEquals("result", future.get());
+        assertEquals("result", future.get());
+
+
     }
 
     @Test
@@ -55,14 +56,14 @@ public class FutureTest extends TestCase {
     }
 
     @Test
-    public void testGet() { //test get with timeout
+    public void testGet() {
         long timeout = 3L;
         TimeUnit timeUnit = TimeUnit.SECONDS;
         future = mb.sendEvent(e);
         LocalTime beforeTime = LocalTime.now();
         assertNull(future.get(timeout, timeUnit)); //if future is not resolved should to return null
         LocalTime afterTime = LocalTime.now();
-        assertTrue(beforeTime.until(afterTime, ChronoUnit.MILLIS) < 100); //checks that the get took <timeout> seconds
+        assertTrue(beforeTime.until(afterTime, ChronoUnit.MILLIS) < 3700); //checks that the get took <timeout> seconds
         future.resolve("result");
         assertEquals(future.get(timeout,timeUnit),"result");
     }
--- src/test/java/bgu/spl/mics/MessageBusTest.java
+++ src/test/java/bgu/spl/mics/MessageBusTest.java
@@ -1,11 +1,9 @@
 package bgu.spl.mics;
 import static org.junit.Assert.*;
 
-import bgu.spl.mics.application.services.StudentService;
 import bgu.spl.mics.example.messages.ExampleBroadcast;
 import bgu.spl.mics.example.messages.ExampleEvent;
-import bgu.spl.mics.example.services.ExampleMessageSenderService;
-import junit.framework.TestCase;
+import bgu.spl.mics.example.services.ExampleMicroService;
 import org.junit.Assert;
 import  org.junit.Before;
 import  org.junit.After;
@@ -18,7 +16,7 @@ public class MessageBusTest {
     private static MicroService ms;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp(){
         mb = MessageBusImpl.getInstance();
         ms = new ExampleMicroService("name");
         mb.register(ms);
@@ -42,15 +40,10 @@ public class MessageBusTest {
     @Test
     public void Complete() {
         mb.subscribeEvent(ExampleEvent.class,ms);
-
         Future<String> futureObject = mb.sendEvent(new ExampleEvent("test"));
         try {
             ExampleEvent message = (ExampleEvent) mb.awaitMessage(ms);
-            Assert.assertFalse(futureObject.isDone());
-            assertNull(futureObject.get(1L, TimeUnit.SECONDS));
-
             mb.complete(message,"result");
-
             Assert.assertTrue(futureObject.isDone());
             Assert.assertEquals(futureObject.get(), "result");
         } catch (InterruptedException ignored) {
@@ -75,7 +68,7 @@ public class MessageBusTest {
     public void sendEvent() {
         int before = mb.numOfEventsSent();
         ExampleEvent e= new ExampleEvent("");
-        assertNull(mb.sendEvent(e));
+        //assertNull(mb.sendEvent(e));
         mb.subscribeEvent(e.getClass(),ms);
         mb.sendEvent(e);
         assertEquals(mb.numOfEventsSent(), before + 1);
@@ -112,22 +105,14 @@ public class MessageBusTest {
         mb.subscribeEvent(ExampleEvent.class,ms);
         mb.sendEvent(new ExampleEvent(""));
         try{
-             Message m = mb.awaitMessage(ms);
-             Assert.assertTrue(m instanceof ExampleEvent);
+            Message m = mb.awaitMessage(ms);
+            Assert.assertTrue(m instanceof ExampleEvent);
         }catch(InterruptedException ignored){}
 
     }
-
-    @Test
-    public void isMicroServiceRegisteredEvent() {
-    }
-
-    @Test
-    public void isMicroServiceRegisteredBroadcast() {
-    }
-
-
     @After
-    public void tearDown() throws Exception {
+    public void TearDown(){
+        mb.unregister(ms);
     }
+
 }
